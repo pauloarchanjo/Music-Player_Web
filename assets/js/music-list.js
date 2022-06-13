ca0cf28d4caafd0f665d616d3583e8334b75c935
Paulo Archanjo <85582352+Hogtear@users.noreply.github.com>
2022-06-13 04:36:28 -0300

    Add files via upload

---
+++ assets/js/music-list.js
@@ -0,0 +1,38 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Flores No.1 - Girassol",
+      artist: "Paulo Archanjo",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Flores No.2 - Ventania",
+      artist: "Paulo Archanjo",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Flores No.3 - Recuerdos de mi Pátria",
+      artist: "Paulo Archanjo",
+      img: "music-3",
+      src: "music-3"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
