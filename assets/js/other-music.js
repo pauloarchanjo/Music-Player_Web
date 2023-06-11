84c1f4150576b99729dee32b11e6a0d5ab602f43
Paulo Archanjo <85582352+pauloarchanjo@users.noreply.github.com>
2023-06-11 02:18:09 -0300

    Push Songs

---
+++ assets/js/other-music.js
@@ -0,0 +1,24 @@
+{
+    name: "Flores No.3 - Cello",
+    artist: "Archanjo",
+    img: "flores-msc-3",
+    src: "flores-msc-3"
+  },
+  {
+    name: "Flores No.4 - Prelúdio",
+    artist: "Archanjo",
+    img: "flores-msc-4",
+    src: "flores-msc-4"
+  },
+  {
+    name: "Flores No.5 - Marenoso",
+    artist: "Archanjo",
+    img: "flores-msc-5",
+    src: "flores-msc-5"
+  },
+  {
+    name: "Flores No.6 - A Brasileira",
+    artist: "Archanjo",
+    img: "flores-msc-6",
+    src: "flores-msc-6"
+  },
